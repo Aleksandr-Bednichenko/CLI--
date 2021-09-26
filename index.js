96dd6a6cb78b3e4505c559702d1e1ac7ffcc02e2
Aleksandr-Bednichenko <a0933093518@gmail.com>
2021-09-26 21:13:40 +0300

    3

--- index.js
+++ index.js
@@ -38,11 +38,6 @@ function invokeAction({ action, id, name, email, phone }) {
 
     case 'add':
       addContact(name, email, phone)
-        // .then((newContact) => {
-        //   // console.log(chalk.green('Add new contact!'))
-        //   console.table(newContact)
-        // })
-        // .catch(console.error)
       break
 
     case 'remove':
